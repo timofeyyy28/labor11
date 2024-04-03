7955f5551d6604fc607d18505f9ec7114fbd7399
Тимофей Матвеев <matveev-timofey@mail.ru>
2024-04-03 20:58:57 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,190 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ClassLibraryLabor10;
+
+namespace Laba11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Часть 1
+            Queue queue = new Queue();
+            for (int i = 0; i < 5; i++)
+            {
+                Musicalinstrument mi1 = new Musicalinstrument();
+                queue.Enqueue(mi1);
+                mi1.RandomInit();
+            }
+            for (int i = 0; i < 5; i++)
+            {
+                Guitar g1 = new Guitar();
+                g1.RandomInit();
+                queue.Enqueue(g1);
+            }
+            foreach (object mi1 in queue)
+            {
+                Console.WriteLine(mi1.ToString());
+            }
+            Console.WriteLine($"Count={queue.Count}");
+
+            // Поиск элемента в очереди
+            Console.WriteLine("Введите элемент для поиска");
+            Musicalinstrument mi2 = new Musicalinstrument();
+            mi2.Init();
+
+            if (queue.Contains(mi2))
+            {
+                Console.WriteLine("Элемент найден");
+            }
+            else
+            {
+                Console.WriteLine("Элемент не найден");
+            }
+
+
+            Console.WriteLine("Удаление первого элемента из очереди");
+            queue.Dequeue();
+            Console.WriteLine("Первый элемент удален");
+            Console.WriteLine($"Count={queue.Count}");
+
+
+            Console.WriteLine("Очередь после удаления элемента:");
+            foreach (object item in queue)
+            {
+                Console.WriteLine(item.ToString());
+            }
+
+
+            Console.WriteLine("Добавление элемента в конец очереди");
+            Musicalinstrument mi3 = new Musicalinstrument();
+            mi3.Init();
+            queue.Enqueue(mi3);
+            Console.WriteLine("Элемент успешно добавлен");
+
+
+            Console.WriteLine("Очередь после добавления элемента:");
+            foreach (object item in queue)
+            {
+                Console.WriteLine(item.ToString());
+            }
+            Console.WriteLine($"Count={queue.Count}");
+
+            // Клонирование очереди с помощью ToArray
+            Queue clonedQueue = new Queue(queue.ToArray());
+
+            Console.WriteLine("Склонированная очередь:");
+            foreach (object item in clonedQueue)
+            {
+                Console.WriteLine(item.ToString());
+            }
+
+            // Сортировка очереди   
+            var array = queue.ToArray();
+            Array.Sort(array);
+
+            // Очищаем исходную очередь
+            queue.Clear();
+
+
+            foreach (var item in array)
+            {
+                queue.Enqueue(item);
+            }
+
+            Console.WriteLine("Отсортированная очередь:");
+            foreach (object item in queue)
+            {
+                Console.WriteLine(item.ToString());
+            }
+            Console.WriteLine("Часть 2");
+            // Часть 2
+            SortedSet<Musicalinstrument> sortedSet = new SortedSet<Musicalinstrument>();
+
+            for (int i = 0; i < 5; i++)
+            {
+                Musicalinstrument mi1 = new Musicalinstrument();
+                sortedSet.Add(mi1);
+                mi1.RandomInit();
+            }
+            for (int i = 0; i < 5; i++)
+            {
+                Guitar g1 = new Guitar();
+                sortedSet.Add(g1);
+                g1.RandomInit();
+            }
+
+
+            foreach (var item in sortedSet)
+            {
+                Console.WriteLine(item.ToString());
+            }
+            Console.WriteLine($"Count={sortedSet.Count}");
+
+
+            Console.WriteLine("Введите элемент для поиска:");
+            Musicalinstrument mi4 = new Musicalinstrument();
+            mi4.Init();
+
+            if (sortedSet.Contains(mi4))
+            {
+                Console.WriteLine("Элемент найден");
+            }
+            else
+            {
+                Console.WriteLine("Элемент не найден");
+            }
+
+            // Удаление первого элемента из множества
+            Console.WriteLine("Удаление первого элемента из множества");
+            sortedSet.Remove(sortedSet.Min);
+            // Удаление первого элемента из множества
+            Console.WriteLine("Удаление последнего элемента из множества");
+            sortedSet.Remove(sortedSet.Max);
+
+            Console.WriteLine("Первый элемент удален");
+            Console.WriteLine($"Count={sortedSet.Count}");
+
+
+            Console.WriteLine("Множество после удаления элемента:");
+            foreach (var item in sortedSet)
+            {
+                Console.WriteLine(item.ToString());
+            }
+
+
+            Console.WriteLine("Добавление элемента в множество");
+            Musicalinstrument mi5 = new Musicalinstrument();
+            mi5.Init();
+            sortedSet.Add(mi5);
+            Console.WriteLine("Элемент успешно добавлен");
+
+
+            Console.WriteLine("Множество после добавления элемента:");
+            foreach (var item in sortedSet)
+            {
+                Console.WriteLine(item.ToString());
+            }
+            Console.WriteLine($"Count={sortedSet.Count}");
+
+            TestCollections testCollections = new TestCollections();
+            testCollections.Get1TimeQueue1();
+            Console.WriteLine("___________");
+            testCollections.Get1TimeQueue2();
+            Console.WriteLine("___________");
+            testCollections.Get1TimeSortedSet1();
+            Console.WriteLine("___________");
+            testCollections.Get1TimeSortedSet2();
+            Console.WriteLine("___________");
+            testCollections.SearchElementNotInCollection();
+            Console.WriteLine("1231234123213213");
+
+        }
+    }
+}
+
---
+++ TestCollections.cs
@@ -0,0 +1,201 @@
+﻿using ClassLibraryLabor10;
+using CustomLibrary;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.Serialization.Formatters;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laba11
+{
+    internal class TestCollections
+    {
+        public Queue<Musicalinstrument> queue1;
+        public Queue<string> queue2;
+        public SortedSet<Musicalinstrument> sortedset1;
+        public SortedSet<string> sortedset2;
+
+        public TestCollections()
+        {
+            queue1 = new Queue<Musicalinstrument>();
+            queue2 = new Queue<string>();
+            sortedset1 = new SortedSet<Musicalinstrument>();
+            sortedset2 = new SortedSet<string>();
+
+            Random rnd = new Random();
+
+            for (int i = 0; i < 1000; i++)
+            {
+                try
+                {
+                    Guitar guitar = new Guitar();
+                    guitar.RandomInit();
+                    Musicalinstrument musicalinstrument = new Musicalinstrument(guitar.Name, guitar.id.number);
+
+                    queue1.Enqueue(musicalinstrument);
+                    queue2.Enqueue(musicalinstrument.ToString());
+                    sortedset1.Add(musicalinstrument);
+                    sortedset2.Add(musicalinstrument.ToString());
+                }
+                catch (Exception e)
+                {
+                    i--;
+                }
+            }
+        }
+
+        public double MeasureSearchTimeCollection1(Musicalinstrument element)
+        {
+            Stopwatch stopwatch = new Stopwatch();
+            long totalTicks = 0;
+
+            for (int i = 0; i < 1000; i++)
+            {
+                stopwatch.Restart();
+                this.queue1.Contains(element);
+                stopwatch.Stop();
+                totalTicks += stopwatch.ElapsedTicks;
+            }
+
+            return (double)totalTicks / 1000;
+        }
+
+        public double MeasureSearchTimeCollection2(string element)
+        {
+            Stopwatch stopwatch = new Stopwatch();
+            long totalTicks = 0;
+
+            for (int i = 0; i < 1000; i++)
+            {
+                stopwatch.Restart();
+                this.queue2.Contains(element);
+                stopwatch.Stop();
+                totalTicks += stopwatch.ElapsedTicks;
+            }
+
+            return (double)totalTicks / 1000;
+        }
+
+        public double MeasureSearchTimeCollection3(Musicalinstrument element)
+        {
+            Stopwatch stopwatch = new Stopwatch();
+            long totalTicks = 0;
+
+            for (int i = 0; i < 1000; i++)
+            {
+                stopwatch.Restart();
+                this.sortedset1.Contains(element);
+                stopwatch.Stop();
+                totalTicks += stopwatch.ElapsedTicks;
+            }
+
+            return (double)totalTicks / 1000;
+        }
+
+        public double MeasureSearchTimeCollection4(string element)
+        {
+            Stopwatch stopwatch = new Stopwatch();
+            long totalTicks = 0;
+
+            for (int i = 0; i < 1000; i++)
+            {
+                stopwatch.Restart();
+                this.sortedset2.Contains(element);
+                stopwatch.Stop();
+                totalTicks += stopwatch.ElapsedTicks;
+            }
+
+            return (double)totalTicks / 1000;
+        }
+        public void Get1TimeQueue1()
+        {
+            // Получение первого элемента (последнего добавленного элемента)
+            Musicalinstrument firstElement = queue1.Peek();
+
+            // Получение последнего элемента (первого добавленного элемента)
+            Musicalinstrument lastElement = queue1.ToArray()[0];
+
+            // Получение центрального элемента
+            Musicalinstrument centralElement = queue1.ToArray()[queue1.Count / 2];
+
+            Console.WriteLine("Время поиска (среднее за 1000 замеров) в очереди 1:");
+            Console.WriteLine($"Первый найден за {MeasureSearchTimeCollection1(firstElement)} тактов");
+            Console.WriteLine($"Последний найден за {MeasureSearchTimeCollection1(lastElement)} тактов");
+            Console.WriteLine($"Центральный найден за {MeasureSearchTimeCollection1(centralElement)} тактов");
+        }
+
+        public void Get1TimeQueue2()
+        {
+            // Получение первого элемента (последнего добавленного элемента)rftyjutyj
+            string firstElement = queue2.Peek();
+
+            // Получение последнего элемента (первого добавленного элемента)
+            string lastElement = queue2.ToArray()[0];
+
+            // Получение центрального элемента
+            string centralElement = queue2.ToArray()[queue2.Count / 2];
+
+            Console.WriteLine("Время поиска (среднее за 1000 замеров) в очереди 2:");
+            Console.WriteLine($"Первый найден за {MeasureSearchTimeCollection2(firstElement)} тактов");
+            Console.WriteLine($"Последний найден за {MeasureSearchTimeCollection2(lastElement)} тактов");
+            Console.WriteLine($"Центральный найден за {MeasureSearchTimeCollection2(centralElement)} тактов");
+        }
+
+        public void Get1TimeSortedSet1()
+        {
+            // Получение первого элемента (наименьшего элемента в отсортированном множестве)
+            Musicalinstrument firstElement = sortedset1.Min;
+
+            // Получение последнего элемента (наибольшего элемента в отсортированном множестве)
+            Musicalinstrument lastElement = sortedset1.Max;
+
+            // Получение центрального элемента
+            int count = sortedset1.Count;
+            Musicalinstrument centralElement = sortedset1.Skip(count / 2).FirstOrDefault();
+
+            Console.WriteLine("Время поиска (среднее за 1000 замеров) в сортированном множестве 1:");
+            Console.WriteLine($"Первый найден за {MeasureSearchTimeCollection3(firstElement)} тактов");
+            Console.WriteLine($"Последний найден за {MeasureSearchTimeCollection3(lastElement)} тактов");
+            Console.WriteLine($"Центральный найден за {MeasureSearchTimeCollection3(centralElement)} тактов");
+        }
+
+        public void Get1TimeSortedSet2()
+        {
+            // Получение первого элемента (наименьшего элемента в отсортированном множестве)
+            string firstElement = sortedset2.Min;
+
+            // Получение последнего элемента (наибольшего элемента в отсортированном множестве)
+            string lastElement = sortedset2.Max;
+
+            // Получение центрального элемента
+            int count = sortedset2.Count;
+            string centralElement = sortedset2.Skip(count / 2).FirstOrDefault();
+
+            Console.WriteLine("Время поиска (среднее за 1000 замеров) в сортированном множестве 2:");
+            Console.WriteLine($"Первый найден за {MeasureSearchTimeCollection4(firstElement)} тактов");
+            Console.WriteLine($"Последний найден за {MeasureSearchTimeCollection4(lastElement)} тактов");
+            Console.WriteLine($"Центральный найден за {MeasureSearchTimeCollection4(centralElement)} тактов");
+            
+        }
+        public void SearchElementNotInCollection()
+        {
+            // Создание элемента, который не входит в коллекцию
+            Musicalinstrument elementNotInCollection = new Musicalinstrument("элемент вне коллекции", 99999);
+
+            // Поиск элемента, который не входит в коллекцию
+            bool foundInQueue1 = this.queue1.Contains(elementNotInCollection);
+            bool foundInQueue2 = this.queue2.Contains(elementNotInCollection.ToString());
+            bool foundInSortedSet1 = this.sortedset1.Contains(elementNotInCollection);
+            bool foundInSortedSet2 = this.sortedset2.Contains(elementNotInCollection.ToString());
+
+            Console.WriteLine("Поиск элемента, который не входит в коллекцию:");
+            Console.WriteLine($"Элемент найден в очереди 1?: {foundInQueue1}");
+            Console.WriteLine($"Элемент найден в очереди 2?: {foundInQueue2}");
+            Console.WriteLine($"Элемент найден в сортированном множестве 1?: {foundInSortedSet1}");
+            Console.WriteLine($"Элемент найден в сортированном множестве 2?: {foundInSortedSet2}");
+        }
+    }
+}
